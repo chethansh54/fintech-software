844ed0372cf86623f7ba4bcf2a2515558495fbc6
chethansh54 <trainerchetanh@gmail.com>
2022-04-10 17:31:40 +0530

    add function

--- src/SimpleInterestCalculator.java
+++ src/SimpleInterestCalculator.java
@@ -5,4 +5,12 @@ public class SimpleInterestCalculator
               p = 13000;  r = 12; t = 2;  
                si  = (p*r*t)/100;   
               System.out.println("Simple Interest is: " +si);  
-    }}  
+    }
+ 
+   public static int add(int num1, int num2) {
+    
+    return num1+num2;
+   }
+
+
+}  
